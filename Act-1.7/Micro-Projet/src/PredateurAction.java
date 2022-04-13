de45089528b9b94fe3819a34698ccee340810b12
oussemajaziri <102968825+oussemajaziri@users.noreply.github.com>
2022-04-13 17:45:54 +0200

    Add files via upload

---
+++ Act-1.7/Micro-Projet/src/PredateurAction.java
@@ -0,0 +1,10 @@
+
+public interface PredateurAction {
+	
+	public abstract void senourrir ();
+	public abstract void chasser ();
+	public abstract void courir ();
+	public abstract void seReproduire ();
+	public abstract void emettreSon ();
+
+}
