4ccd1167d51e003be2ae38e6aafcf846b5f6f923
oussemajaziri <102968825+oussemajaziri@users.noreply.github.com>
2022-04-13 16:46:26 +0100

    Delete Act-1.7/Micro-Projet directory

--- Act-1.7/Micro-Projet/src/LionAction.java
+++
@@ -1,8 +0,0 @@
-
-public interface LionAction {
-
-	public abstract void afficheCaracteristiques ();
-	public abstract void entendreSon ();
-	public abstract void seSepareDuGroupe ();
-	
-}
--- Act-1.7/Micro-Projet/src/OursAction.java
+++
@@ -1,8 +0,0 @@
-
-public interface OursAction {
-	
-	public abstract void afficheCaracteristiques ();
-	public abstract void grimper ();
-	public abstract void creuser ();
-	public abstract void hiverner ();
-}
