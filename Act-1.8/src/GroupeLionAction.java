d045ba9acf0531027d996e76fce791977fed6d67
oussemajaziri <102968825+oussemajaziri@users.noreply.github.com>
2022-04-14 18:10:23 +0100

    Delete Act-1.8 directory

--- Act-1.8/src/GroupeLionAction.java
+++
@@ -1,8 +0,0 @@
-
-public interface GroupeLionAction {
-	
-	public void constituerCoupleDominant ();
-	public void reproductionLion ();
-	public void déclareLionNonDominant ();
-	
-}
--- Act-1.8/src/GroupePredateurAction.java
+++
@@ -1,9 +0,0 @@
-
-public interface GroupePredateurAction {
-
-	public abstract void afficheCaractéristiqueGroupe ();
-	public abstract void afficheCaractéristiquesMembres ();
-	public abstract void ajoutMembre (Predateur predateur);
-	public abstract void enleverMembre(Predateur predateur);
-	
-}
